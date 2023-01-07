d7eb479ec2ce02f838020a9397501674966e3e4d
Nick Hoang <nickjhoang@gmail.com>
2023-01-06 18:40:56 -0600

    created the html for 30 different pokemon cards with the data pulled from the API using JS

--- main.js
+++ main.js
@@ -1,106 +1,72 @@
 const pokemon = 'https://pokeapi.co/api/v2/pokemon/';
-const card = document.getElementById("card");
+const mainContainer = document.getElementById("main-container");
 
-let getPokemon = () => {
-    let id = Math.floor(Math.random() * 150) + 1;
+let pokeStorage = [];
+let pokeFavorites = [];
+let counter = 0;
+let num = 0;
 
-    const finalURL = pokemon + id;
+let catchPokemon = () => {
+    let counter = 0;
+    while(counter < 30) {
+        let id = Math.floor(Math.random() * 150) + 1;
+        const finalURL = pokemon + id;
+        
+        fetch(finalURL)
+            .then((response) => response.json())
+            .then((data) => {
+                // console.log(data);
+                storePokemon(data);
+            });
+        counter ++;}
+    };
 
-    fetch(finalURL)
-        .then((response) => response.json())
-        .then((data) => {
-            generateCard(data);
-    });
+let storePokemon = (data) => {
+    let caughtPokemon = {
+        pokeName: data.name.toUpperCase(),
+        pokeID: data.id,
+        imgSrc: data.sprites.other.dream_world.front_default,
+        type: data.types[0].type.name,
+        hp: data.stats[0].base_stat,
+        attackStat: data.stats[1].base_stat
+    };
+    pokeStorage.push(caughtPokemon);
+    generateCard();
 };
 
-let generateCard = (data) => {
-    console.log(data);
-    const hp = data.stats[0].base_stat;
-    const imgSrc = data.sprites.other.dream_world.front_default;
-    const pokeName = data.name.toUpperCase();
-    const attackStat = data.stats[1].base_stat;
-    const type = data.types[0].type.name;
-    
-    card.innerHTML = `
-            <div class="poke-content">
-                <div class="poke-name">
-                    <span>${pokeName}</span>
+console.log(pokeStorage);
+
+let generateCard = () => {
+        document.getElementById("main-container").insertAdjacentHTML("beforeend",
+            `<div class="container">
+                <div class="poke-card" id=card>
+                    <div class="poke-content">
+                        <div class="poke-name">
+                            <span>${pokeStorage[num].pokeName}</span>
+                        </div>
+                        <div class="img-wrapper">
+                            <img src=${pokeStorage[num].imgSrc} alt="">
+                        </div>
+                        <div class="stats">
+                            <ul class="stat-list">
+                                <li class="stat-type value">
+                                    <span>Type</span>
+                                    <span class="replace1">${pokeStorage[num].type}</span>
+                                </li>
+                                <li class="stat-hp value">
+                                    <span>HP</span>
+                                    <span class="replace2">${pokeStorage[num].hp}</span>
+                                </li>
+                                <li class="stat-attack value">
+                                    <span>Attack</span>
+                                    <span class="replace3">${pokeStorage[num].attackStat}</span>
+                                </li>
+                            </ul> 
+                        </div>
+                    </div>
                 </div>
-                <div class="img-wrapper">
-                    <img src=${imgSrc} alt="">
-                </div>
-                <div class="stats">
-                    <ul class="stat-list">
-                        <li class="stat-type value">
-                            <span>Type</span>
-                            <span class="replace1">${type}</span>
-                        </li>
-                        <li class="stat-hp value">
-                            <span>HP</span>
-                            <span class="replace2">${hp}</span>
-                        </li>
-                        <li class="stat-attack value">
-                            <span>Attack</span>
-                            <span class="replace3">${attackStat}</span>
-                        </li>
-                    </ul> 
-                </div>
-            </div>
-    `
-}
+            </div>`)
+    num++;
+};
 
-console.log(getPokemon());
-// const card = document.getElementById("card");
-
-// let getPokemon = () => {
-//     fetch(url)
-//     .then((response) => response.json())
-//     .then((data) =>  )
-// } 
-
-
-/* PLANNING 
-
-1. GET DATA FROM API
-> how do i get data from API? 
-> do i need multiple API calls to retrieve multiple types of data? 
-
-2. CREATE A FUNCTION THAT CREATES HTML BASED ON DATA I GET FROM API
-> this will need to input data for the following fields 
-    -- pokemon name
-    -- image
-    -- type
-    -- hp
-    -- attack
-
-3. REPEAT THIS FUNCTION 30 TIMES TO DISPLAY 30 CARDS IN DOM
-> create a function that will loop until the 30 card threshold is met
-> store this in a collection array (objects within an array)
-
-4. CREATE A FAVORITES COLLECTION
-> create a "favorite button" on the card that will push it from the collections array to the 
-    favorite's array
-> create a click event that if the user unclicks the card, once in the favorites array, it will remove and return it
-    to the collections array
-
-5. Build toggle button that will sort the cards alphabetically
-
-6. Display the total sum of some piece of data in the array
-    -- maybe total team health points? or attack? 
-
-7. Make responsive
-
-
-
-
-
-
-
-
-
-*/ 
-
-
-
-
-// console.log(pokemon);
+catchPokemon();
