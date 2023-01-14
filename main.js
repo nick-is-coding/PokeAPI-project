b313db2d4538f2fc170f5a9583f04aaecfe890ce
Nick Hoang <nickjhoang@gmail.com>
2023-01-14 17:06:38 -0600

    fixed functionality of sort button, add CSS to overall project, make sure it is mobile responsive, make sum of total hp and attack pts on the favorites page

--- main.js
+++ main.js
@@ -1,32 +1,54 @@
 const pokemon = 'https://pokeapi.co/api/v2/pokemon/';
 const mainContainer = document.getElementById("main-container");
 const originalContainer = document.getElementById("original-container");
+const favoriteContainer = document.getElementById("my-favorites");
+
 let sortToggle = document.getElementById("sort-button");
+let favToggle = document.getElementById("sort-fave");
+let theSum = document.getElementById("hp-sum");
+let theAttack = document.getElementById("attack-sum");
 
 let pokeStorage = [];
+let favoriteStorage = [];
 let counter = 0;
 let num = 0;
 let isAscending = true;
+let sum = 0;
+let attack = 0;
 
-let buttonFunctionality = () => {
+let totalStats = () => {
+    theSum.textContent = `${sum}`;
+    theAttack.textContent = `${attack}`;
+}
+
+let buttonFunctionality = (poke) => {
     const moveButton = document.getElementById(`move-button-${pokeStorage[num].pokeID}`);
-    const container2 = document.getElementById('my-favorites');
     const blockToMove = document.getElementById(`blockToMove-${pokeStorage[num].pokeID}`);
-    const addText = 'ADD TO TEAM';
-    const removeText = 'REMOVE FROM TEAM';
+    const addText = 'add to team';
+    const removeText = 'remove from team';
     let cardState = 'not-favorite';
 
     moveButton.addEventListener("click", () => {
         if (cardState === "not-favorite") {
             moveButton.setAttribute("data-state", "favorite");
             moveButton.textContent = removeText;
-            container2.appendChild(blockToMove);
+            favoriteContainer.appendChild(blockToMove);
             cardState = "favorite";
+            favoriteStorage.push(poke);
+            pokeStorage.splice(pokeStorage.indexOf(poke), 1);
+            sum += poke.hp;
+            attack += poke.attackStat;
+            totalStats();
         } else {
             moveButton.removeAttribute("data-state", "favorite");
             moveButton.textContent = addText;
             originalContainer.appendChild(blockToMove);
             cardState = "not-favorite";
+            pokeStorage.push(poke);
+            favoriteStorage.splice(favoriteStorage.indexOf(poke), 1);
+            sum -= poke.hp;
+            attack -= poke.attackStat;
+            totalStats();
         }
     });
 };
@@ -45,27 +67,27 @@ let generateCard = (poke) => {
                     <div class="stats">
                         <ul class="stat-list">
                             <li class="stat-type value">
-                                <span>Type</span>
-                                <span class="replace1">${poke.type}</span>
+                                <span>type</span>
+                                <span>${poke.type}</span>
                             </li>
                             <li class="stat-hp value">
-                                <span>HP</span>
-                                <span class="replace2">${poke.hp}</span>
+                                <span>hp</span>
+                                <span>${poke.hp}</span>
                             </li>
                             <li class="stat-attack value">
-                                <span>Attack</span>
-                                <span class="replace3">${poke.attackStat}</span>
+                                <span>attack</span>
+                                <span>${poke.attackStat}</span>
                             </li>
                         </ul> 
                         <div class="heart-shape empty">
-                            <button id="move-button-${poke.pokeID}" class="fave-button">ADD TO TEAM</button>
+                            <button id="move-button-${poke.pokeID}" class="fave-button">add to team</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>`
         );
-    buttonFunctionality();
+    buttonFunctionality(poke);
     num++;
 };
 
@@ -98,7 +120,7 @@ let catchPokemon = () => {
 };
 
 let runSort = () => {
-    sortToggle.addEventListener('click', sortCards = () => {
+    num = 0;
         pokeStorage.sort((pokeA, pokeB) => {
             let nameA = pokeA.pokeName.toLowerCase();
             let nameB = pokeB.pokeName.toLowerCase();
@@ -122,9 +144,9 @@ let runSort = () => {
         isAscending = !isAscending;
 
         if(isAscending) {
-            sortToggle.innerHTML = "Sort A-Z";
+            sortToggle.innerHTML = "sort a-z";
         } else {
-            sortToggle.innerHTML = "Sort Z-A";
+            sortToggle.innerHTML = "sort z-a";
         }
 
         pokeStorage.forEach((poke, index) => {
@@ -132,8 +154,43 @@ let runSort = () => {
             card.getElementsByClassName("poke-name")[0].textContent = poke.pokeName;
             originalContainer.appendChild(card);
         });
-    });
 };
 
+let sortFaves = () => {
+        num = 0;
+        favoriteStorage.sort((pokeA, pokeB) => {
+            let nameA = pokeA.pokeName.toLowerCase();
+            let nameB = pokeB.pokeName.toLowerCase();
+            if (isAscending) {
+                if (nameA < nameB) {
+                    return -1;
+                } else if (nameA > nameB) {
+                    return 1;
+                }
+                return 0;
+            } else {
+                if (nameA > nameB) {
+                    return -1;
+                } else if (nameA < nameB) {
+                    return 1;
+                }
+                return 0;
+            }
+        });
+
+        isAscending = !isAscending;
+
+        if(isAscending) {
+            favToggle.innerHTML = "sort a-z";
+        } else {
+            favToggle.innerHTML = "sort z-a";
+        }
+
+        favoriteStorage.forEach((poke, index) => {
+            let card = document.getElementById(`blockToMove-${poke.pokeID}`);
+            card.getElementsByClassName("poke-name")[0].textContent = poke.pokeName;
+            favoriteContainer.appendChild(card);
+        });
+};
 
 catchPokemon();
